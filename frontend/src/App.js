1d4e046cacbfabef17254af29bf87c683ceba476
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-11-12 23:40:28 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home';
+import Audio from './components/Audio';
 import Medication from './components/Medication/Medication';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/medication" element={<Medication />} />
+        <Route path="/audio" element={<Audio />} />
       </Routes>
     </BrowserRouter>
   );
